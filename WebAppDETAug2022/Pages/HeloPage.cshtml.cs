bd3390f8fe9e64cd48989760694053893240b25b
kishor <kishorgururaj2000@gmail.com>
2022-09-09 09:20:15 +0530

    webApp

---
+++ WebAppDETAug2022/Pages/HeloPage.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography.X509Certificates;
+using WebAppDETAug2022.Servics;
+
+namespace WebAppDETAug2022.Pages
+{
+    public class HeloPageModel : PageModel
+    {
+            public Helo2 ob { get; set; }
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            ob = new Helo2();
+                Message = ob.SayHelo("kishor");
+        }
+
+    }
+    
+}
---
+++ WebAppDETAug2022/Servics/IHelo.cs
@@ -0,0 +1,24 @@
+﻿namespace WebAppDETAug2022.Servics
+{
+    public interface IHelo
+    {
+        string SayHelo(string name);
+    }
+
+    public class Helo1 : IHelo
+    {
+        public string SayHelo(string name)
+        {
+            return $"Helo{name},welcome to ASP>Net";
+        }
+
+    }
+    public class Helo2 : IHelo
+    {
+        public string SayHelo(string name)
+        {
+            return $"Hai,Helo{name},Howz the day!!!";
+        }
+
+    }
+}
