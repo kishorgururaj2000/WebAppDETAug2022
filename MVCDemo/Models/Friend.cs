d031d2b471da2c3eb11dad7d53443d97b8566cf6
kishor <kishorgururaj2000@gmail.com>
2022-09-09 17:22:51 +0530

    today

---
+++ MVCDemo/Models/Friend.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCDemo.Models
+{
+    public class Friend
+    {
+        //[Required]
+        public int FriendId { get; set; }
+
+       // [Required]
+        public string FriendName { get; set; }
+        public string Place { get; set; }
+
+    }
+}
